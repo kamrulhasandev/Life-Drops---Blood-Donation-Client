e51e3001c657d77fec28cb8c869800e4182bbe0d
kamrul <kamrul444216@gmail.com>
2024-05-23 06:44:09 +0600

    customize axios interceptors

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,8 +1,9 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { axiosBaseQuery } from "@/helpers/axios/axiosBaseQuery";
+import { createApi } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({
+  baseQuery: axiosBaseQuery({
     baseUrl: "http://localhost:5000/api/v1",
   }),
   endpoints: () => ({}),
---
+++ src/types/common.ts
@@ -0,0 +1,21 @@
+export type IMeta = {
+  page: number;
+  limit: number;
+  total: number;
+};
+
+export type ResponseSuccessType = {
+  data: any;
+  meta?: IMeta;
+};
+
+export type IGenericErrorResponse = {
+  statusCode: number;
+  message: string;
+  errorMessages: IGenericErrorMessage[];
+};
+
+export type IGenericErrorMessage = {
+  path: string | number;
+  message: string;
+};
