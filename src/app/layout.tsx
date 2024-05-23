722a135f1cca90e0105bd1bdf32aba0f1b43d79e
kamrul <kamrul444216@gmail.com>
2024-05-23 06:21:32 +0600

    setup redux and redux query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
+import Providers from "@/lib/Providers/Provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,13 +17,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <>
-          <Toaster position="top-center" />
-          {children}
-        </>
-      </body>
-    </html>
+    <Providers>
+      <html lang="en">
+        <body className={inter.className}>
+          <>
+            <Toaster position="top-center" />
+            {children}
+          </>
+        </body>
+      </html>
+    </Providers>
   );
 }
